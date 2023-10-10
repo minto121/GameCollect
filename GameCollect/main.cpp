8081e6d2d28ac85defbb6f7c1d618dfcc332453c
minto121 <tomohide-matsuda-k22h@stu.kbc.ac.jp>
2023-10-10 13:51:02 +0900

    main

---
+++ GameCollect/main.cpp
@@ -0,0 +1,71 @@
+#include"DxLib.h"
+#include"SceneManager.h"
+#include"PadInput.h"
+#include "Title.h"
+#include "FpsController.h"
+
+
+#define FRAMERATE 60.0 //フレームレート
+
+#define SCREEN_HEIGHT 720	//画面サイズ (縦)
+#define SCREEN_WIDTH 1280	//画面サイズ (横)
+#define SCORE_UI_SIZE 180	//スコア表示エリアサイズ (横)
+
+
+/***********************************************
+ * プログラムの開始
+ ***********************************************/
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	SetMainWindowText("GameCollect");
+
+	ChangeWindowMode(TRUE);		// ウィンドウモードで起動
+
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32);	//画面サイズの設定
+
+	if (DxLib_Init() == -1) return -1;	// DXライブラリの初期化処理
+
+	SetDrawScreen(DX_SCREEN_BACK);	// 描画先画面を裏にする
+
+	SceneManager* sceneMng;
+
+	try
+	{
+		sceneMng = new SceneManager((AbstractScene*)new Title());
+
+	}
+	catch (const char* err)
+	{
+		FILE* fp = NULL;
+
+		DATEDATA data;
+
+		GetDateTime(&data);
+		//ファイルオープン
+		fopen_s(&fp, "ErrLog.txt", "a");
+		//エラーデータの書き込み
+		fprintf_s(fp, "%02d年 %02d月 %02d日 %02d時 %02d分 %02d秒 : %sがありません。\n", data.Year, data.Mon, data.Day, data.Hour, data.Min, data.Sec, err);
+
+		return 0;
+	}
+	FpsController FPSC(FRAMERATE, 800);
+
+	// ゲームループ
+	while ((ProcessMessage() == 0) && (sceneMng->Update() != nullptr)) {
+
+		ClearDrawScreen();		// 画面の初期化
+		PAD_INPUT::UpdateKey();
+		sceneMng->Draw();
+		FPSC.All();
+		FPSC.Disp();
+		//強制終了
+		if (PAD_INPUT::OnButton(XINPUT_BUTTON_BACK))
+		{
+			break;
+		}
+
+		ScreenFlip();			// 裏画面の内容を表画面に反映
+	}
+	return 0;
+}
