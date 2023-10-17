f734c7029a5feddd25f5786ac6b852b5fb288b3b
Ryoya-Kouchi <120348450+Ryoya-Kouchi@users.noreply.github.com>
2023-10-17 15:01:37 +0900

    a

--- GameCollect/Title.cpp
+++ GameCollect/Title.cpp
@@ -4,6 +4,7 @@
 #include "PadInput.h"
 //#include "Help.h"
 //#include "End.h"
+#include"Select.h"
 
 //タイトルメニュー
 enum class TITLE_MENU
@@ -20,8 +21,8 @@ Title::Title()
 	//Select = 0;
 	//Once = TRUE;
 
-	////フォントの追加
-	//MenuFont = CreateFontToHandle("HG創英角ﾎﾟｯﾌﾟ体", 64, 8, DX_FONTTYPE_ANTIALIASING);
+	//フォントの追加
+	MenuFont = CreateFontToHandle("HG創英角ﾎﾟｯﾌﾟ体", 64, 8, DX_FONTTYPE_ANTIALIASING);
 
 	////タイトル画像の読み込み
 	//if ((TitleImg = LoadGraph("Resource/Images/mori.png")) == -1)
@@ -33,39 +34,12 @@ Title::Title()
 	//{
 	//	throw "Resource/Images/apple.png";
 	//}
-	////BGMの読み込み
-	//if ((TitleBGM = LoadSoundMem("Resource/sounds/BGM/yonhonnorecorder.wav")) == -1)
-	//{
-	//	throw "Resource/sounds/BGM/yonhonnorecorder.wav";
-	//}
-	////BGMの音量変更
-	//ChangeVolumeSoundMem(140, TitleBGM);
-
-	////SEの読み込み
-	//if ((MenuSE = LoadSoundMem("Resource/sounds/SE/select01.wav")) == -1) //選択SE
-	//{
-	//	throw "Resource/sounds/SE/select01.wav";
-	//}
-	////SEの音量変更
-	//ChangeVolumeSoundMem(110, MenuSE);
-
-	////BGMの再生
-	//if (CheckSoundMem(TitleBGM) == 0)
-	//{
-	//	PlaySoundMem(TitleBGM, DX_PLAYTYPE_LOOP);
-	//}
-
+	
 }
 
 Title::~Title()
 {
-	//フォントの削除
-	DeleteFontToHandle(MenuFont);
-
-	StopSoundMem(TitleBGM);
-	//サウンドの削除
-	DeleteSoundMem(TitleBGM);
-	DeleteSoundMem(MenuSE);
+	
 }
 
 AbstractScene* Title::Update()
@@ -109,34 +83,37 @@ AbstractScene* Title::Update()
 		Once = TRUE;
 	}
 
-	if (PAD_INPUT::OnButton(XINPUT_BUTTON_A))
-	{
-		//switch (static_cast<TITLE_MENU>(Select))
-		//{
-		//	//ゲーム画面へ
-		//case TITLE_MENU::GAME_START:
-
-		//	return new GameMain();
-		//	break;
-		//	//ランキング画面へ
-		//case TITLE_MENU::GAME_RANKING:
-		//	return new DrawRanking();
-		//	//StopSoundMem(TitleBGM);
-		//	break;
-		//	//ヘルプ画面
-		//case TITLE_MENU::GAME_HELP:
-		//	return new Help();
-		//	//StopSoundMem(TitleBGM);
-		//	break;
-		//	//エンド画面へ
-		//case TITLE_MENU::GAME_END:
-		//	return new End();
-		//	//StopSoundMem(TitleBGM);
-		//	break;
-		//default:
-		//	break;
-		//}
+	if (CheckHitKey(KEY_INPUT_0)) {
+		return new  GameSelect();
 	}
+
+	//if (PAD_INPUT::OnButton(XINPUT_BUTTON_A))
+	//{
+	//	switch (static_cast<TITLE_MENU>(Select))
+	//	{
+	//		//ゲーム画面へ
+	//	case TITLE_MENU::GAME_START:
+
+	//		return new GameMain();
+	//		break;
+	//		//ランキング画面へ
+	//	case TITLE_MENU::GAME_RANKING:
+	//		return new DrawRanking();
+	//		//StopSoundMem(TitleBGM);
+	//		break;
+	//		//ヘルプ画面
+	//	case TITLE_MENU::GAME_HELP:
+	//		return new Help();
+	//		//StopSoundMem(TitleBGM);
+	//		break;
+	//		//エンド画面へ
+	//	case TITLE_MENU::GAME_END:
+	//		return new End();
+	//		//StopSoundMem(TitleBGM);
+	//		break;
+	//	default:
+	//		break;
+	//	}
 	return this;
 }
 
@@ -144,7 +121,7 @@ void Title::Draw()const
 {
 	//タイトルの描画
 	DrawGraph(0, 0, TitleImg, FALSE);
-	DrawStringToHandle(150, 100, "りんごおとし", 0xffffff, MenuFont);
+	DrawStringToHandle(150, 100, "ゲーム大全", 0xffffff, MenuFont);
 
 	//メニューの描画
 	DrawStringToHandle(730, 240, "すたーと", 0xffffff, MenuFont);
--- GameCollect/main.cpp
+++ GameCollect/main.cpp
@@ -3,6 +3,7 @@
 #include"PadInput.h"
 #include "Title.h"
 #include "FpsController.h"
+#include"Hanahuda_GameMain.h"
 
 
 #define FRAMERATE 60.0 //フレームレート
