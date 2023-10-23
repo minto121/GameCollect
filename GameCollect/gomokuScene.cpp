33ef4907e3bf0bff3cad49a5f529ac87b8c8a2b0
manato0115 <120348237+Tom0412@users.noreply.github.com>
2023-10-23 11:01:44 +0900

    描画テスト

--- GameCollect/gomokuScene.cpp
+++ GameCollect/gomokuScene.cpp
@@ -1,1 +1,20 @@
 #include "gomokuScene.h"
+#include"DxLib.h"
+
+gomokuScene::gomokuScene()
+{
+	TitleImg = LoadGraph("../images/Gomoku/gomoku.png");
+}
+
+gomokuScene::~gomokuScene()
+{}
+
+AbstractScene* gomokuScene::Update()
+{
+	return this;
+}
+
+void gomokuScene::Draw() const
+{
+	DrawGraph(280, 0, TitleImg, TRUE);
+}
--- GameCollect/gomokuScene.h
+++ GameCollect/gomokuScene.h
@@ -2,6 +2,12 @@
 #include "AbstractScene.h"
 class gomokuScene : public AbstractScene
 {
-
+private:
+	int TitleImg;
+public:
+	gomokuScene();
+	~gomokuScene();
+	AbstractScene* Update() override;
+	void Draw() const override;
 };
 
