b6dc0e4a2e990a2319eaa3c08a31f37e52f75ed9
yoshiki-nakama <104492110+yoshiki-nakama@users.noreply.github.com>
2023-10-23 12:16:58 +0900

    正解をランダムで出すVer.1.0

--- GameCollect/Hit&Blow.cpp
+++ GameCollect/Hit&Blow.cpp
@@ -1,21 +1,23 @@
 #include"Hit&Blow.h"
 #include"DxLib.h"
+#include<stdlib.h>
+#include<time.h>
 
 HitAndBlow::HitAndBlow() 
 {
 	// ダミー画像を読込
-	LoadGraph("../images/HitAndBlow/BG_Dummy.png");
+	/*LoadGraph("../images/HitAndBlow/BG_Dummy.png");*/
 	// ヒットした時の画像読込
-	HitImg=LoadGraph("../images/HitAndBlow/ヒットピン1.png");
+	HitImg = LoadGraph("../images/HitAndBlow/ヒットピン.png");
 	// ブローした時の画像読込
-	BlowImg=LoadGraph("../images/HitAndBlow/ブローピン.png");
+	BlowImg = LoadGraph("../images/HitAndBlow/ブローピン.png");
 	// 色の画像追加(仮表示)
-	Color[0] = LoadGraph("../images/HitAndBlow/青色の駒1.png");
-	Color[1] = LoadGraph("../images/HitAndBlow/赤色の駒1.png");
-	Color[2] = LoadGraph("../images/HitAndBlow/緑色の駒1.png");
-	Color[3] = LoadGraph("../images/HitAndBlow/黄色の駒1.png");
-	Color[4] = LoadGraph("../images/HitAndBlow/紫色の駒1.png");
-	Color[5] = LoadGraph("../images/HitAndBlow/白色の駒.png");
+	ColorImg[0] = LoadGraph("../images/HitAndBlow/青色の駒.png");
+	ColorImg[1] = LoadGraph("../images/HitAndBlow/赤色の駒.png");
+	ColorImg[2] = LoadGraph("../images/HitAndBlow/緑色の駒.png");
+	ColorImg[3] = LoadGraph("../images/HitAndBlow/黄色の駒.png");
+	ColorImg[4] = LoadGraph("../images/HitAndBlow/紫色の駒.png");
+	ColorImg[5] = LoadGraph("../images/HitAndBlow/白色の駒.png");
 }
 
 HitAndBlow::~HitAndBlow()
@@ -25,12 +27,28 @@ HitAndBlow::~HitAndBlow()
 
 AbstractScene* HitAndBlow::Update()
 {
+	/* 答えの配列をランダムに設定する */
+	srand((unsigned int)time(NULL));
+
+	for (int i = 0; i < 4; i++) {
+		Answer[i] = rand() % 6;
+		if (Answer[i] == Answer[(i + 1) % 4] || Answer[i] == Answer[(i + 2) % 4] || Answer[i] == Answer[(i + 3) % 4]) {
+			i--;
+		}
+	}
 	return this;
 }
 
 void HitAndBlow::Draw() const
 {
-	for (int i = 0; i < 6; i++) {
-		DrawGraph(200 + i * 75, 200, Color[i], TRUE); // それぞれの色の駒を表示
+	for (int i = 0; i < 6; i++) { // 駒を表示
+		DrawGraph(350 + i * 100, 600, ColorImg[i], TRUE); // それぞれの色の駒を表示(位置は決定)
 	}
+	DrawGraph(200, 200, HitImg, TRUE); // それぞれの色の駒を表示
+	DrawGraph(200, 250, BlowImg, TRUE); // それぞれの色の駒を表示
+
+	for (int i = 0; i < 4; i++) {
+		DrawFormatString(400, 400 + i * 20, 0xffffff, "%d", Answer[i]);
+	}
+	
 }
--- GameCollect/Hit&Blow.h
+++ GameCollect/Hit&Blow.h
@@ -3,7 +3,10 @@
 
 class HitAndBlow : public AbstractScene {
 private:
+	int ColorImg[6]; // 色の駒の画像配列
 	int Color[6]; // 色の駒を入れる配列
+	int Answer[4];  // 色と場所を当てる配列
+	int Reasoning[4];  // 色と場所を推理する配列 
 	int Hit;     // ヒット数を数える変数
 	int Blow;    // ブロー数を数える変数
 	int HitImg;  // ヒットピンの画像表示
