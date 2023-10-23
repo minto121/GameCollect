8e8837880820516a3cc8ed2baee57f93fd0f19bc
yafusosyun <122337129+yafusosyun@users.noreply.github.com>
2023-10-23 09:50:32 +0900

    mu

---
+++ GameCollect/sinkeisuijaku.cpp
@@ -0,0 +1,10 @@
+#include "sinkeisuijaku.h"
+#include "DxLib.h"
+void sinkeisuijaku::Update()
+{
+	S_timg = LoadGraph("");
+}
+
+void sinkeisuijaku::Draw()
+{
+}
---
+++ GameCollect/sinkeisuijaku.h
@@ -0,0 +1,16 @@
+#pragma once
+class sinkeisuijaku
+{
+public:
+
+private:
+
+	sinkeisuijaku();
+
+	void Update();
+
+	void Draw();
+
+	int S_timg;
+};
+
