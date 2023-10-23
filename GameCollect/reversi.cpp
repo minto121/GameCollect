69aedb1fa516c0ce367063c2348802ff8541d6fe
minto121 <tomohide-matsuda-k22h@stu.kbc.ac.jp>
2023-10-23 10:09:48 +0900

    revesi

--- GameCollect/reversi.cpp
+++ GameCollect/reversi.cpp
@@ -1,1 +1,18 @@
-#include "reversi.h"
+#include "Reversi.h"
+
+Revesi::Revesi()
+{
+}
+
+Revesi::~Revesi()
+{
+}
+
+AbstractScene* Revesi::Update()
+{
+	return this;
+}
+
+void Revesi::Draw() const
+{
+}
--- GameCollect/reversi.h
+++ GameCollect/reversi.h
@@ -1,5 +1,26 @@
 #pragma once
-class reversi
+#include "AbstractScene.h"
+
+class Revesi :
+    public AbstractScene
+
 {
+private:
+
+
+
+public:
+    //コンストラクタ
+    Revesi();
+
+    //デストラクタ
+    ~Revesi();
+
+    //描画以外の更新を実行
+    AbstractScene* Update() override;
+
+    //描画に関することを実装
+    void Draw() const override;
 };
+
 
