98827ad103328caab86d6ac4aaba7ee4b2ab6cbb
yoshiki-nakama <104492110+yoshiki-nakama@users.noreply.github.com>
2023-10-24 11:26:25 +0900

    画像ファイルの名前変更

--- GameCollect/Hit&Blow.cpp
+++ GameCollect/Hit&Blow.cpp
@@ -1,5 +1,6 @@
 #include"Hit&Blow.h"
 #include"DxLib.h"
+#include"PadInput.h"
 #include<stdlib.h>
 #include<time.h>
 
@@ -8,16 +9,22 @@ HitAndBlow::HitAndBlow()
 	// ダミー画像を読込
 	/*LoadGraph("../images/HitAndBlow/BG_Dummy.png");*/
 	// ヒットした時の画像読込
-	HitImg = LoadGraph("../images/HitAndBlow/ヒットピン.png");
+	HitImg = LoadGraph("../images/HitAndBlow/HitPin.png");
 	// ブローした時の画像読込
-	BlowImg = LoadGraph("../images/HitAndBlow/ブローピン.png");
+	BlowImg = LoadGraph("../images/HitAndBlow/BlowPin.png");
 	// 色の画像追加(仮表示)
-	ColorImg[0] = LoadGraph("../images/HitAndBlow/青色の駒.png");
-	ColorImg[1] = LoadGraph("../images/HitAndBlow/赤色の駒.png");
-	ColorImg[2] = LoadGraph("../images/HitAndBlow/緑色の駒.png");
-	ColorImg[3] = LoadGraph("../images/HitAndBlow/黄色の駒.png");
-	ColorImg[4] = LoadGraph("../images/HitAndBlow/紫色の駒.png");
-	ColorImg[5] = LoadGraph("../images/HitAndBlow/白色の駒.png");
+	ColorImg[0] = LoadGraph("../images/HitAndBlow/Blue_Foal.png");
+	ColorImg[1] = LoadGraph("../images/HitAndBlow/Red_Foal.png");
+	ColorImg[2] = LoadGraph("../images/HitAndBlow/Green_Foal.png");
+	ColorImg[3] = LoadGraph("../images/HitAndBlow/Yellow_Foal.png");
+	ColorImg[4] = LoadGraph("../images/HitAndBlow/Purple_Foal.png");
+	ColorImg[5] = LoadGraph("../images/HitAndBlow/White_Foal.png");
+
+	DecisionFlg = TRUE;
+
+	WarpPosition = 0;
+
+	Turns = 0;
 }
 
 HitAndBlow::~HitAndBlow()
@@ -27,15 +34,53 @@ HitAndBlow::~HitAndBlow()
 
 AbstractScene* HitAndBlow::Update()
 {
-	/* 答えの配列をランダムに設定する */
-	srand((unsigned int)time(NULL));
+	RandomDecision(); // 答えの配列をランダムに設定する
 
-	for (int i = 0; i < 4; i++) {
-		Answer[i] = rand() % 6;
-		if (Answer[i] == Answer[(i + 1) % 4] || Answer[i] == Answer[(i + 2) % 4] || Answer[i] == Answer[(i + 3) % 4]) {
-			i--;
-		}
-	}
+	/* ここに自分が駒を入れる処理を書く */
+	/* 重すぎてタスクの応答がなくなるため、コメントアウト中 */
+	//while (Turns < 8)
+	//{
+	//	//十字キー↑入力
+	//	if (PAD_INPUT::OnButton(XINPUT_BUTTON_DPAD_UP))
+	//	{
+	//		WarpPosition--;
+	//		if (WarpPosition < 0) WarpPosition = 0;  // 位置が0より下なら、0にする
+	//	}
+
+	//	// 十字キー↓入力
+	//	if (PAD_INPUT::OnButton(XINPUT_BUTTON_DPAD_DOWN))
+	//	{
+	//		WarpPosition++;
+	//		if (WarpPosition > 4) WarpPosition = 4;
+	//	}
+
+	//	// 十字キー←入力
+	//	if (PAD_INPUT::OnButton(XINPUT_BUTTON_DPAD_LEFT))
+	//	{
+	//		Color[SidePosition--];
+	//		if (SidePosition < 0) SidePosition = 6;
+	//	}
+
+	//	//十字キー→入力
+	//	if (PAD_INPUT::OnButton(XINPUT_BUTTON_DPAD_RIGHT))
+	//	{
+	//		Color[SidePosition++];
+	//		if (SidePosition > 6) SidePosition = 0;
+	//	}
+
+	//	if (PAD_INPUT::OnButton(XINPUT_BUTTON_A && WarpPosition == 0))
+	//	{
+	//		/* ジャッジ処理を書く */
+	//		Turns++;
+	//	}
+	//	else if (PAD_INPUT::OnButton(XINPUT_BUTTON_A && WarpPosition != 0))
+	//	{
+	//		Reasoning[WarpPosition] = Color[SidePosition];  // 色を場所に配置
+	//	}
+
+	//}
+
+		
 	return this;
 }
 
@@ -44,11 +89,32 @@ void HitAndBlow::Draw() const
 	for (int i = 0; i < 6; i++) { // 駒を表示
 		DrawGraph(350 + i * 100, 600, ColorImg[i], TRUE); // それぞれの色の駒を表示(位置は決定)
 	}
-	DrawGraph(200, 200, HitImg, TRUE); // それぞれの色の駒を表示
-	DrawGraph(200, 250, BlowImg, TRUE); // それぞれの色の駒を表示
+	DrawGraph(200, 100, HitImg, TRUE); // それぞれの色の駒を表示
+	DrawGraph(300, 100, BlowImg, TRUE); // それぞれの色の駒を表示
 
-	for (int i = 0; i < 4; i++) {
-		DrawFormatString(400, 400 + i * 20, 0xffffff, "%d", Answer[i]);
-	}
+	/* 正解の駒表示 */
+	/* 重すぎてタスクの応答がなくなるため、コメントアウト中 */
+	//for (int i = 0; i < 4; i++) {
+	//	DrawFormatString(400, 400 + i * 20, 0xffffff, "%d", Answer[i]); // デバック用
+	//	DrawGraph(1000, 170 + i * 75, ColorImg[Answer[i]], TRUE); // 答えを画像で表示
+	//	DrawGraph(100 * Turns, 170 + i * 75, ColorImg[Reasoning[WarpPosition]], TRUE); // 予想を画像で表示
+	//}
 	
+}
+
+void HitAndBlow::RandomDecision() 
+{
+	/* 答えの配列をランダムに設定する */
+	srand((unsigned int)time(NULL));
+
+	if (DecisionFlg == TRUE) {
+		for (int i = 0; i < 4; i++) {
+			Answer[i] = rand() % 6;
+			if (Answer[i] == Answer[(i + 1) % 4] || Answer[i] == Answer[(i + 2) % 4] || Answer[i] == Answer[(i + 3) % 4]) {
+				i--;
+			}
+		}
+		DecisionFlg = FALSE;
+	}
+
 }
--- GameCollect/Hit&Blow.h
+++ GameCollect/Hit&Blow.h
@@ -3,14 +3,23 @@
 
 class HitAndBlow : public AbstractScene {
 private:
+	/* 画像読み込み */
 	int ColorImg[6]; // 色の駒の画像配列
+	int HitImg;  // ヒットピンの画像表示
+	int BlowImg; // ブローピンの画像表示
+
+	/* 変数 */
 	int Color[6]; // 色の駒を入れる配列
 	int Answer[4];  // 色と場所を当てる配列
 	int Reasoning[4];  // 色と場所を推理する配列 
+	int DecisionFlg;  // 答え決定フラグ
 	int Hit;     // ヒット数を数える変数
 	int Blow;    // ブロー数を数える変数
-	int HitImg;  // ヒットピンの画像表示
-	int BlowImg; // ブローピンの画像表示
+	int WarpPosition;  // 今入れようとしている場所の変数
+	int SidePosition;  // 色を選択する場所の変数
+	int Change;  // 交換する時に使う変数
+	int Turns;
+	
 
 public:
 	// インクリメント
@@ -21,4 +30,7 @@ public:
 	AbstractScene* Update();
 	// 描画をアップデート
 	void Draw() const override;
+
+	// ランダムに正解の色と場所決める関数
+	void RandomDecision();
 };
