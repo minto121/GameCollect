3e74bf7bb954cfcf296b4ee60422daf17c354833
matsutake-taketo <120348208+matsutake-taketo@users.noreply.github.com>
2023-10-24 11:04:42 +0900

    ランダム数字入れた

--- GameCollect/Porker.cpp
+++ GameCollect/Porker.cpp
@@ -5,7 +5,24 @@
 Porker::Porker()
 {
     if ((Back = LoadGraph("../GameCollect/images/TexasHoldem/BG_Dummy.png"))) {}
-    if (LoadDivGraph("../GameCollect/images/TexasHoldem/Trannpu.png", 10, 13, 4, 75, 105, Tranpu_Img)) {}
+    /*if (LoadDivGraph("../GameCollect/images/TexasHoldem/", 52, 13, 4, 100, 150, Tranpu_Img,FALSE)) {}*/
+
+    P_rand1 = GetRand(51);   //プレイヤーのホールカード1
+    P_rand2 = GetRand(51);   //プレイヤーのホールカード2
+
+    E_rand1 = GetRand(51);   //敵のホールカード1
+    E_rand2 = GetRand(51);   //敵のホールカード2
+
+    C_rand1 = GetRand(51);   //コミュニティカード1
+    C_rand2 = GetRand(51);   //コミュニティカード2
+    C_rand3 = GetRand(51);   //コミュニティカード3
+    C_rand4 = GetRand(51);   //コミュニティカード4
+    C_rand5 = GetRand(51);   //コミュニティカード5
+
+    C_flg1 = FALSE;
+    C_flg2 = FALSE;
+    C_flg3 = FALSE;
+   
 }
 
 Porker::~Porker()
@@ -15,13 +32,118 @@ Porker::~Porker()
 
 AbstractScene* Porker::Update()
 {
+
+    //               カードのかぶり防止               //
+
+    //ホールカード1がかぶった時やり直し用(プレイヤー)
+    if (P_rand2 == P_rand1) {
+        P_rand2 = GetRand(51);
+    }
+   
+    //ホールカード1がかぶった時やり直し用(敵)
+    if (E_rand1 == P_rand1 || E_rand1 == P_rand2) {
+        E_rand1 = GetRand(51);
+    }
+    //ホールカード2がかぶった時やり直し用(敵)
+    if (E_rand2 == P_rand1 || E_rand2 == P_rand2|| E_rand2 == E_rand1) {
+        E_rand2 = GetRand(51);
+    }
+    
+    //コミュニティカードがかぶった時やり直し用(1回目)
+    if (C_rand1 == P_rand1 || C_rand1 == P_rand2 || C_rand1 == E_rand1 || C_rand1 == E_rand2) {
+        C_rand1 = GetRand(51);
+    }
+
+    //コミュニティカードがかぶった時やり直し用(2回目)
+    if (C_rand2== P_rand1 || C_rand2 == P_rand2 || C_rand2 == E_rand1 || C_rand2 == E_rand2 || C_rand2 == C_rand1) {
+        C_rand2 = GetRand(51);
+    }
+
+    //コミュニティカードがかぶった時やり直し用(3回目)
+    if (C_rand3 == P_rand1 || C_rand3 == P_rand2 || C_rand3 == E_rand1 || C_rand3 == E_rand2 || C_rand3 == C_rand1 || C_rand3==C_rand2) {
+        C_rand3 = GetRand(51);
+    }
+
+    //コミュニティカードがかぶった時やり直し用(4回目)
+    if (C_rand4 == P_rand1 || C_rand4 == P_rand2 || C_rand4 == E_rand1 || C_rand4 == E_rand2 || C_rand4 == C_rand1 || C_rand4 == C_rand2||C_rand4 == C_rand3) {
+        C_rand4 = GetRand(51);
+    }
+
+    //コミュニティカードがかぶった時やり直し用(5回目)
+    if (C_rand5 == P_rand1 || C_rand5 == P_rand2 || C_rand5 == E_rand1 || C_rand5 == E_rand2 || C_rand5 == C_rand1 || C_rand5 == C_rand2 || C_rand5 == C_rand3||C_rand5==C_rand4) {
+        C_rand5 = GetRand(51);
+    }
+
+
+
+
+    //           コールするかどうかのフラグ管理              //
+
+    //コール(今はA押す)
+    if (CheckHitKey(KEY_INPUT_A)) {
+        C_flg1 = TRUE;
+    }
+   
+    //コール(今はS押す)
+    if (CheckHitKey(KEY_INPUT_S)) {
+        C_flg2 = TRUE;
+    }
+
+    //コール(今はD押す)
+    if (CheckHitKey(KEY_INPUT_D)) {
+        C_flg3 = TRUE;
+    }
+
+
+
     return this;
 }
 
 void Porker::Draw() const
 {
-    DrawGraph(0, 0, Back, TRUE);
-    DrawString(70, 240, "Porker", 0xffffff);
-    DrawGraph(400, 450, Tranpu_Img[1], TRUE);
+
+    //                   カードの表示               //
+
+    DrawFormatString(400, 600, 0xffffff, "prand1: %d", P_rand1, TRUE); //プレイヤーのホールカード1
+    DrawFormatString(600, 600, 0xffffff, "prand2: %d", P_rand2, TRUE); //プレイヤーのホールカード2
+
+    DrawFormatString(400, 50, 0xffffff, "erand1: %d", E_rand1, TRUE); //敵のホールカード1
+    DrawFormatString(600, 50, 0xffffff, "erand2: %d", E_rand2, TRUE); //敵のホールカード2
+
+    //1回目コールした時コミュニティカードを三枚出す
+    if (C_flg1 == TRUE) {
+        DrawFormatString(100, 300, 0xffffff, "crand1: %d", C_rand1, TRUE); //コミュニティカード1
+        DrawFormatString(300, 300, 0xffffff, "crand2: %d", C_rand2, TRUE); //コミュニティカード2
+        DrawFormatString(500, 300, 0xffffff, "crand3: %d", C_rand3, TRUE); //コミュニティカード3
+    }
+
+    //2回目コールした時コミュニティカードを1枚出す
+    if (C_flg2 == TRUE) {
+        DrawFormatString(700, 300, 0xffffff, "crand4: %d", C_rand4, TRUE); //コミュニティカード4
+    }
+
+    //3回目コールした時コミュニティカードを1枚出す
+    if (C_flg3 == TRUE) {
+        DrawFormatString(900, 300, 0xffffff, "crand5: %d", C_rand5, TRUE); //コミュニティカード4
+    }
+
+
+
+    //               キー表示              //
+
+    if (C_flg1 == FALSE) {
+        DrawString(500, 650, "PRESS [A] KEY", 0xffffff, TRUE);
+    }
+
+    if (C_flg1 == TRUE&&C_flg2 == FALSE) {
+        DrawString(500, 650, "PRESS [S] KEY", 0xffffff, TRUE);
+    }
+
+    if (C_flg1 == TRUE && C_flg2 == TRUE&&C_flg3 == FALSE) {
+        DrawString(500, 650, "PRESS [D] KEY", 0xffffff, TRUE);
+    }
+
+   /* DrawGraph(0, 0, Back, TRUE);*/
+   /* DrawGraph(400, 450, Tranpu_Img[1], TRUE);*/
    
 }
--- GameCollect/Porker.h
+++ GameCollect/Porker.h
@@ -5,6 +5,19 @@ class Porker : public AbstractScene
 private:
 	int Tranpu_Img[52];
 	int Back;
+	int P_rand1;
+	int P_rand2;
+	int E_rand1;
+	int E_rand2;
+	int C_rand1;
+	int C_rand2;
+	int C_rand3;
+	int C_rand4;
+	int C_rand5;
+
+	int C_flg1;
+	int C_flg2;
+	int C_flg3;
 public:
 	Porker();
 
