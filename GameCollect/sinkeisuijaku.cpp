b8c57aec33fd0691fef1464d90efdb01f84c0ba3
yafusosyun <122337129+yafusosyun@users.noreply.github.com>
2023-10-24 09:42:16 +0900

    トランプのサイズ調整

--- GameCollect/sinkeisuijaku.cpp
+++ GameCollect/sinkeisuijaku.cpp
@@ -12,10 +12,17 @@ AbstractScene* sinkeisuijaku::Update()
 void sinkeisuijaku::Draw() const
 {
 	DrawGraph(0,0, S_back, FALSE);
-	DrawBox(450,100, 120, 120, 0x00ffff, FALSE);
+	
 	for (int j = 1; j < 5; j++) {
 		for (int i = 0; i < 5; i++) {
-			DrawRotaGraph(450 + i * 100, 0+ j*150, 0.5, 0, S_timg, TRUE);
+			DrawRotaGraph(450 + i * 110, 0+ j*160, 0.5, 0, S_timg, TRUE);
+		}
+	}
+	DrawBox(395, 90, 495, 235, 0x00ffff, FALSE);
+
+	for (int j = 0; j < 5; j++) {
+		for (int i = 0; i < 5; i++) {
+			DrawBox(395 + i * 110, 90 + j * 160, 495 + i * 110, 235 + j * 160, 0x00ffff, FALSE);
 		}
 	}
 }
