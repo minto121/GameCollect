67abe2caee0156eb2197ad42a942908aee6a31bf
モトラ２０２２ <sorato-motomura-k22f@stu.kbc.ac.jp>
2023-10-26 12:05:12 +0900

    画像が読みまれない
    ボールの画像が反映されない

--- GameCollect/SixBollPuzzle.cpp
+++ GameCollect/SixBollPuzzle.cpp
@@ -4,8 +4,12 @@
 
 SixBollPuzzle::SixBollPuzzle()
 {
+	//éläpÇÃògâÊëúì«çû
 	Back_Ground_img = LoadGraph("../images/SixBallPuzzle/square.png");
-	Back_Ground_img = LoadGraph("../images/SixBallPuzzle/ball.png");
+	//
+	Ball_img = LoadGraph("../images/SixBallPuzzle/boll.png");
+
+	//if (LoadDivGraph("images/boll.png", 3, 3, 3, 160, 200, Ball_img) == -1);
 
 }
 
@@ -22,6 +26,5 @@ AbstractScene* SixBollPuzzle::Update()
 void SixBollPuzzle::Draw() const
 {
 	DrawGraph(0, 0, Back_Ground_img, FALSE);
-	DrawGraph(0, 0, Ball_img, FALSE);
-
+	DrawGraph(0, 0, Ball_img, TRUE);
 }
