ef3df83e6658e5553709d9d16a8031ef8849d309
minto121 <tomohide-matsuda-k22h@stu.kbc.ac.jp>
2023-11-01 13:06:51 +0900

    reversi

--- GameCollect/reversi.cpp
+++ GameCollect/reversi.cpp
@@ -6,7 +6,10 @@
 #define WHITE 1
 #define BLACK 2
 
+//”Õ–Ê
 #define SIZ 10
+#define BOARD_IMG_SIZE 640;
+#define CELL_SIZE = BOARD_IMG_SIZE /SIZ;
 
 int board[SIZ][SIZ];
 
@@ -20,10 +23,8 @@ Reversi::Reversi()
 	/*CursorPoint = { 0, 0 };*/
 
 	K_Flg = FALSE;
-
-	bor = board[SIZ][SIZ];
-
-	Board();
+	M_Flg = TRUE;
+	
 }
 
 Reversi::~Reversi()
@@ -34,7 +35,8 @@ Reversi::~Reversi()
 AbstractScene* Reversi::Update()
 {
 	turn();
-	Board();
+
+	if(GetMouseInput()&MOUSE_INPUT_LEFT)
 
 	////ƒJ[ƒ\ƒ‹‚ðãˆÚ“®‚³‚¹‚é
 	//if (PAD_INPUT::OnButton(XINPUT_BUTTON_DPAD_UP) || (PAD_INPUT::GetLStick().ThumbY > 10000 && YOnce == TRUE)) {
@@ -129,6 +131,7 @@ AbstractScene* Reversi::Update()
 void Reversi::Draw() const
 {
 	DrawFormatString(0, 100, GetColor(255, 255, 255), " %d:button", K_Flg);
+	DrawFormatString(0, 140, GetColor(255, 255, 255), " %d:click", );
 	//‹î‚Ì•`‰æ
 	if (K_Flg == TRUE)
 	{
@@ -138,19 +141,20 @@ void Reversi::Draw() const
 	//”Õ–Ê‚Ì•`‰æ
 	DrawGraph(340, 50, R_Img, TRUE);
 
-}
 
-void Reversi::print_board(int board[SIZ][SIZ])
-{
-	int i, j;
-
-	for (i = 1; i <= 8; i++) {
-		for (j = 1; j <= 8; j++) {
-			DrawCircle(0,0,0x000fff, bor,TRUE);
-		}
-		printf("\n");
-	}
 }
+//
+//void Reversi::print_board(int board[SIZ][SIZ])
+//{
+//	int i, j;
+//
+//	for (i = 1; i <= 8; i++) {
+//		for (j = 1; j <= 8; j++) {
+//			DrawCircle(0,0,0x000fff, bor,TRUE);
+//		}
+//		printf("\n");
+//	}
+//}
 
 void Reversi::init_board(int board[SIZ][SIZ])
 {
@@ -170,58 +174,58 @@ void Reversi::init_board(int board[SIZ][SIZ])
 	board[4][4] = board[5][5] = 2;
 }
 
-void Reversi::Board()
-{
-
-	int i;
-	int j;
-
-	//for (int x = 0; x < 10;x++) {
-	//	for (int y = 0; y < 10; y++) {
-	//		Boa.typ[x][y] = 0;
-	//		Boa.x[x][y] = 0;
-	//		Boa.y[x][y] = 0;
-	//	}
-	//}
-
-	//Boa.typ[3][3] = 1;
-	//Boa.x[3][3] = 3 * 100;
-	//Boa.y[3][3] = 3 * 100;
-
-	//Boa.typ[4][4] = 1;
-	//Boa.x[4][4] = 4 * 100;
-	//Boa.y[4][4] = 4 * 100;
-
-	//Boa.typ[3][4] = 2;
-	//Boa.x[3][4] = 4 * 100;
-	//Boa.y[3][4] = 4 * 100;
-
-	//Boa.typ[4][3] = 2;
-	//Boa.x[4][3] = 4 * 100;
-	//Boa.y[4][3] = 3 * 100;
-
-	static int initdata[10][10]{
- { -1 , -1, -1, -1, -1, -1, -1, -1, -1, -1},
- { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
- { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
- { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
- { -1 , 0 , 0 , 0 , 1 , 2 , 0 , 0 , 0 , -1},
- { -1 , 0 , 0 , 0 , 2 , 1 , 0 , 0 , 0 , -1},
- { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
- { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
- { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
- { -1 , -1, -1, -1, -1, -1, -1, -1, -1, -1}
-	};
-
-	for (i = 0; i < SIZ; i++) {
-		for (j = 0; j < SIZ; j++) {
-			board[i][j] = initdata[i][j];
-		}
-	}
-	board[3][3] = board[4][4] = 1;
-	board[3][4] = board[4][3] = 2;
-
-}
+//void Reversi::Board()
+//{
+//
+//	int i;
+//	int j;
+//
+//	//for (int x = 0; x < 10;x++) {
+//	//	for (int y = 0; y < 10; y++) {
+//	//		Boa.typ[x][y] = 0;
+//	//		Boa.x[x][y] = 0;
+//	//		Boa.y[x][y] = 0;
+//	//	}
+//	//}
+//
+//	//Boa.typ[3][3] = 1;
+//	//Boa.x[3][3] = 3 * 100;
+//	//Boa.y[3][3] = 3 * 100;
+//
+//	//Boa.typ[4][4] = 1;
+//	//Boa.x[4][4] = 4 * 100;
+//	//Boa.y[4][4] = 4 * 100;
+//
+//	//Boa.typ[3][4] = 2;
+//	//Boa.x[3][4] = 4 * 100;
+//	//Boa.y[3][4] = 4 * 100;
+//
+//	//Boa.typ[4][3] = 2;
+//	//Boa.x[4][3] = 4 * 100;
+//	//Boa.y[4][3] = 3 * 100;
+//
+//	static int initdata[10][10]{
+// { -1 , -1, -1, -1, -1, -1, -1, -1, -1, -1},
+// { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
+// { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
+// { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
+// { -1 , 0 , 0 , 0 , 1 , 2 , 0 , 0 , 0 , -1},
+// { -1 , 0 , 0 , 0 , 2 , 1 , 0 , 0 , 0 , -1},
+// { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
+// { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
+// { -1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -1},
+// { -1 , -1, -1, -1, -1, -1, -1, -1, -1, -1}
+//	};
+//
+//	for (i = 0; i < SIZ; i++) {
+//		for (j = 0; j < SIZ; j++) {
+//			board[i][j] = initdata[i][j];
+//		}
+//	}
+//	board[3][3] = board[4][4] = 1;
+//	board[3][4] = board[4][3] = 2;
+//
+//}
 
 
 
