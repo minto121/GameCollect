56f76a5c43d507ee9264022aab4cddd3cb72e7e3
yoshiki-nakama <104492110+yoshiki-nakama@users.noreply.github.com>
2023-11-01 14:46:14 +0900

    おかしいので、いったん戻す

--- GameCollect/main.cpp
+++ GameCollect/main.cpp
@@ -3,8 +3,15 @@
 #include"PadInput.h"
 #include "Title.h"
 #include "FpsController.h"
+<<<<<<< HEAD
 #include"Hanafuda_GameMain.h"
 
+=======
+#include"Hanahuda_GameMain.h"
+#include "Chekkermain.h"
+#include"Mankara.h"
+#include"takoyaki.h"
+>>>>>>> parent of c99f340 (逕ｻ蜒上�ｮ霑ｽ蜉)
 #define FRAMERATE 60.0 //フレームレート
 
 #define SCREEN_HEIGHT 720	//画面サイズ (縦)
