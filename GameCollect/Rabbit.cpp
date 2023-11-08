b724a13c60c782cbaed89926e45869ef48252dea
Kotoribako <youto-tome-k22h@stu.kbc.ac.jp>
2023-11-08 19:27:53 +0900

    a

--- GameCollect/Rabbit.cpp
+++ GameCollect/Rabbit.cpp
@@ -2,6 +2,8 @@
 #include "DxLib.h"
 #include "PadInput.h"
 #include "Rabbit.h"
+//#include <windows.h>
+//#include <d3d11.h>
 
 Rabbit::Rabbit()
 {
@@ -10,13 +12,16 @@ Rabbit::Rabbit()
 	//3Dモデルの読込
 	HoundImg = MV1LoadModel("Dog_Model.mv1");
 	RabbitImg = MV1LoadModel("Rabbit_Model.mv1");
+
+	// テクスチャの読み込み
+	int textureHandle = LoadGraph("images/RabbitAndHounds/Textures/Coloe_Textures/T_PigHead_00.TGA");
 }
 
 Rabbit::~Rabbit()
 {
-	// モデルハンドルの削除
-	MV1DeleteModel(HoundImg);
-	MV1DeleteModel(RabbitImg);
+	//// モデルハンドルの削除
+	//MV1DeleteModel(HoundImg);
+	//MV1DeleteModel(RabbitImg);
 }
 
 AbstractScene* Rabbit::Update()
@@ -24,8 +29,13 @@ AbstractScene* Rabbit::Update()
 	//ウサギ座標
 	rabbit_X = 100, rabbit_Y = 100;
 
+	if (PAD_INPUT::OnButton(KEY_INPUT_RIGHT) == true)
+	{
+		rabbit_X = 200;
+	}
+
 	// ウサギを移動
-	if (CheckHitKey(KEY_INPUT_UP)) {
+	/*if (CheckHitKey(KEY_INPUT_UP)) {
 		rabbit_Y -= rabbitSpeed;
 	}
 	if (CheckHitKey(KEY_INPUT_DOWN)) {
@@ -36,7 +46,7 @@ AbstractScene* Rabbit::Update()
 	}
 	if (CheckHitKey(KEY_INPUT_RIGHT)) {
 		rabbit_X += rabbitSpeed;
-	}
+	}*/
 
 	return this;
 }
@@ -49,21 +59,20 @@ void Rabbit::Draw() const
 	//タイトル
 	DrawString(10, 20, "プレイヤー操作はウサギ", 0x000000);
 
-	SetCameraPositionAndTarget_UpVecY(VGet(0, 0, -1000), VGet(0, 0, 0));
+	//SetCameraPositionAndTarget_UpVecY(VGet(0, 0, -1000), VGet(0, 0, 0));
 
-	// 画面に映る位置に３Ｄモデルを移動
-	MV1SetPosition(HoundImg, VGet(320.0f, -300.0f, 600.0f));
-	MV1SetPosition(RabbitImg, VGet(320.0f, -300.0f, 600.0f));
+	//// 画面に映る位置に３Ｄモデルを移動
+	//MV1SetPosition(HoundImg, VGet(320.0f, -300.0f, 600.0f));
+	//MV1SetPosition(RabbitImg, VGet(320.0f, -300.0f, 600.0f));
 
-	//3Dモデルの描画
-	MV1DrawModel(HoundImg);
-	MV1DrawModel(RabbitImg);
+	////3Dモデルの描画
+	//MV1DrawModel(HoundImg);
+	//MV1DrawModel(RabbitImg);
 
-	////ウサギと猟犬を描画
-	//DrawBox(rabbit_X, rabbit_Y, rabbit_X - 1, rabbit_Y - 1, GetColor(0, 0, 255), RabbitImg);
-	//DrawCircle(hound_X, hound_Y, hound_X - 1, hound_Y - 1, GetColor(0, 0, 255), TRUE);
+	//ウサギと猟犬を描画
+	DrawBox(200 + rabbit_X, 250 + rabbit_Y, 300 + rabbit_X - 1, 350 + rabbit_Y - 1, GetColor(0, 0, 255), RabbitImg);
+	DrawCircle(hound_X, hound_Y, hound_X - 1, hound_Y - 1, GetColor(0, 0, 255), HoundImg);
 
-	DrawBox(300, 350, 400, 450, GetColor(255, 0, 0), TRUE);
-	DrawCircle(900, 400, 60, GetColor(0, 0, 255), TRUE);
-
+	/*DrawBox(300, 350, 400, 450, GetColor(255, 0, 0), RabbitImg);
+	DrawCircle(900, 400, 60, GetColor(0, 0, 255), TRUE);*/
 }
--- GameCollect/Rabbit.h
+++ GameCollect/Rabbit.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "AbstractScene.h"
 
+
 class Rabbit : public AbstractScene
 {
 private:
