b1f62842d6bb2f54fb54bc006a9c4a7b4b26e2e6
miyaguni-rikuto <120348141+miyaguni-rikuto@users.noreply.github.com>
2023-11-16 11:25:57 +0900

    カードの裏面表示

--- GameCollect/takoyaki.cpp
+++ GameCollect/takoyaki.cpp
@@ -7,12 +7,25 @@
 
 Takoyaki::Takoyaki()
 {
-	Select = 0;
+	Select=0;
 	Cards_img[56];
-	cardimg = LoadDivGraph("../images/Takoyaki/PlayingCards.png",56,14,4,128,256,Cards_img);//カード画像読み込み
-	CursolImg = LoadGraph("../images/Takoyaki/cursor.png");//カーソル画像読み込み
+	cardimg = LoadDivGraph("../images/Takoyaki/PlayingCards.png",56,14,4,128,256,Cards_img);	//カード画像読み込み
+	CursolImg = LoadGraph("../images/Takoyaki/cursor.png");										//カーソル画像読み込み
 	select_X = 100;
 	BackCard_Img = LoadGraph("../image/Takoyaki/card_back.png");
+	
+
+	//手札の初期化
+	for (int i = 0; i < 10; ++i) {
+		handCard[0][i] = 0;
+		handCard[1][i] = 0;
+		cardFlipped[0][i] = false;
+		cardFlipped[1][i] = false;
+	}
+
+	//最初に手札を描画
+	Draw();
+	ScreenFlip();
 }
 
 Takoyaki::~Takoyaki() {
@@ -21,18 +34,6 @@ Takoyaki::~Takoyaki() {
 
 AbstractScene* Takoyaki::Update()
 {
-
-	/*if (PAD_INPUT::OnPressed(XINPUT_BUTTON_DPAD_LEFT))
-	{
-		Select--;
-		select_X -= 5;
-	}
-
-	if (PAD_INPUT::OnPressed(XINPUT_BUTTON_DPAD_RIGHT)) {
-		Select++;
-		select_X += 5;
-	}*/
-	//十字キー↑入力
 	if (PAD_INPUT::OnButton(XINPUT_BUTTON_DPAD_LEFT))
 	{
 		Select--;
@@ -46,44 +47,55 @@ AbstractScene* Takoyaki::Update()
 	}
 	if (PAD_INPUT::OnButton(XINPUT_BUTTON_A)) {
 		if (select_X == 100) {
-			cardFlipped = true; // 特定の位置でAボタンを押した場合、カードを裏返す
-			ClearDrawScreen(); // 画面をクリア
+			cardFlipped[0][Select] = !cardFlipped[0][Select]; // カードを裏返す
 		}
 	}
+
+	//手札の描画
+	Draw();
+	ScreenFlip();
 	return this;
 }
 
 void Takoyaki::Draw()const
 {
+	ClearDrawScreen();
 
-	//タイトルの描画
-	//DrawGraph(0, 0, TitleImg, FALSE);
-	DrawString(350, 50, "main", 0xffffff, TRUE);
 
-	//カード画像描画
-	if (cardFlipped) {
-		DrawGraph(70, 50, Cards_img[1], TRUE);
+	// 上の手札を描画
+	for (int i = 0; i < 10; ++i) {
+		DrawGraph(70 + i * 120, 50, cardFlipped[0][i] ? Cards_img[handCard[0][i]] : BackCard_Img, TRUE);
 	}
-	{
-		
-		if (cardFlipped) {
-			DrawGraph(70, 50, Cards_img[0], TRUE);
-		}
-		else {
-			DrawGraph(70, 50, BackCard_Img, TRUE); // カードが裏返っていない場合は裏面を描画
-		}
-		
+
+	// 下の手札を描画
+	for (int i = 0; i < 10; ++i) {
+		DrawGraph(70 + i * 120, 300, Cards_img[handCard[1][i]], TRUE);
 	}
-	DrawGraph(70, 50, Cards_img[0], TRUE);
-	DrawGraph(190, 50, Cards_img[2], TRUE);
-	DrawGraph(190, 50, Cards_img[3], TRUE);
-	DrawGraph(190, 50, Cards_img[4], TRUE);
-	DrawGraph(190, 50, Cards_img[5], TRUE);
+
+	////カード画像描画
+	//if (cardFlipped) {
+	//	DrawGraph(70, 50, Cards_img[1], TRUE);
+	//}
+	//{
+	//	
+	//	if (cardFlipped) {
+	//		DrawGraph(70, 50, Cards_img[0], TRUE);
+	//	}
+	//	else {
+	//		DrawGraph(70, 50, BackCard_Img, TRUE); // カードが裏返っていない場合は裏面を描画
+	//	}
+	//	
+	//}
+	//DrawGraph(70, 50, Cards_img[0], TRUE);
+	//DrawGraph(190, 50, Cards_img[2], TRUE);
+	//DrawGraph(190, 50, Cards_img[3], TRUE);
+	//DrawGraph(190, 50, Cards_img[4], TRUE);
+	//DrawGraph(190, 50, Cards_img[5], TRUE);
 
 
 	//カーソルの描画
 	DrawGraph(select_X, 250, CursolImg, TRUE);
-
+	ScreenFlip();
 
 	//DrawGraph(650, select_y, CursorImg, TRUE);
 }
--- GameCollect/takoyaki.h
+++ GameCollect/takoyaki.h
@@ -6,13 +6,16 @@ class Takoyaki :
 {
 
 private:
-    int Select;     //選択数 
-    int Cards_img[56];  //カード
-    int CursolImg; //カーソル画像
-    int select_X;//カードごとのカーソル位置を保持
+    int Select;             //選択数 
+    int Cards_img[56];      //カード情報格納配列
+    int CursolImg;          //カーソル画像
+    int cardimg;            //カード
+    int select_X;           //カードごとのカーソル位置を保持
     int BackCard_Img;
-    bool cardFlipped;//カードの裏返し
-    int cardimg;
+    bool cardFlipped[2][10];       //カードの裏返し
+   
+    //手札の情報を保持する配列
+    int handCard[2][10];
 public:
     //コンストラクタ
     Takoyaki();
@@ -21,10 +24,10 @@ public:
     ~Takoyaki();
 
     //描画以外の更新を実行
-    AbstractScene* Update() override;
+    AbstractScene* Update();
 
     //描画に関することを実装
-    void Draw() const override;
+    void Draw() const;
 };
 
 
