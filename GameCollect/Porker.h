e441c867c06a7d7b7d2daa53661246d0e58e971a
yoshiki-nakama <104492110+yoshiki-nakama@users.noreply.github.com>
2023-12-05 09:46:52 +0900

    Merge branch 'Yoshiki-Hit&Blow' into LastCard

---
+++ GameCollect/Porker.h
@@ -0,0 +1,147 @@
+#pragma once
+#include "AbstractScene.h"
+class Porker : public AbstractScene
+{
+private:
+	
+	int a;
+	int wt;
+	int ps1;
+	int es1;
+	int Bs;
+	int BTN_flg;
+	int P_FPS;
+	int P_TEKI;
+	int WP;
+	int WP2;
+	int WP3;
+	int WP4;
+	int WP_FLG[20];
+	int YP[31];
+	int YP2[31];
+	
+
+	int Winflg;
+	int Loseflg;
+	int Drawflg;
+	int E_Fouldflg;
+	int E_CAllflg;
+	int E_Raiseflg;
+	int P_Call_H_flg;
+	int P_Raise_H_flg;
+	int P_Fould_H_flg;
+	int E_Fould_H_flg;
+	int G_Over_H_flg;
+	int WTflg[4];
+	int WT2flg[3];
+
+	int WTRelese[4];
+	
+	int BTN_RELESE_FLG[6];
+	
+	int P_F_flg;
+	int RC_H_flg;
+	int CARD_URA_H_flg;
+
+	int Tranpu_Img[56];
+	int Back;
+	int P_rand[2];
+	
+	int E_rand[2];
+	
+	int C_rand[5];
+	
+	int C_flg[4];
+
+	int P_CARD_S[2];//プレイヤーカードの商格納用
+	int P_CARD_A[2];//プレイヤーカードの余り格納用
+
+	int E_CARD_S[2];//敵カードの商格納用
+	int E_CARD_A[2];//敵カードの余り格納用
+
+	int C_CARD_S[5];//場に出ているカード1~5の商格納用
+	int C_CARD_A[5];//場に出ているカード1~5の余り格納用
+	
+	int P_SORT[16];
+	int E_SORT[16];
+	int WINandLOSE[2];
+	int R_Win_FLG[2];
+
+	int P_PEA_FLG;//プレイヤーの1ペアのフラグ
+	int P_2PEA_FLG;//プレイヤーの2ペアのフラグ
+	int P_3CARD_FLG;//プレイヤーのスリーカードのフラグ
+	int P_FH_FLG;
+	int P_FLASH_FLG;
+	int P_STRAIGHT_FLG;
+	int P_4CARD_FLG;
+	int P_SF_FLG;
+	int P_RSF_FLG;
+
+	int E_PEA_FLG;//
+	int E_2PEA_FLG;//
+	int E_3CARD_FLG;//
+	int E_FH_FLG;
+	int E_FLASH_FLG;
+	int E_STRAIGHT_FLG;
+	int E_4CARD_FLG;
+	int E_SF_FLG;
+	int E_RSF_FLG;
+
+public:
+	Porker();
+
+	~Porker();
+
+	//描画以外の更新を実行
+	 AbstractScene* Update() override;
+
+	//描画に関することを実装
+	 void Draw() const override;
+
+
+	
+	void ROUND_INIT();//ラウンド初期化
+
+	void CARD_SETTING();//カードの呼び出し用関数
+
+	void CARD_SETTING2();//カードの呼び出し用関数
+
+	void PLAYER_CALL();//プレイヤーのコール用関数
+
+	void PLAYER_RAISE();//プレイヤーのレイズ用関数
+
+	void ENEMIE_CHOISE();//敵の選択用関数
+
+	void CARD_ANALYSIS();//カードの数字とマークを調べる用の関数
+
+	void P_CARD_PEA();//1,2ペア判断のための関数
+
+	void P_CARD_3_CARD();//3カードのための関数
+
+	void P_CARD_4_CARD();//3カードのための関数
+
+	void P_CARD_FLASH();//フラッシュのための関数
+
+	void P_CARD_STRAIGHT();//ストレートのための関数
+
+	void P_CARD_STRAIGHTFALSH();//ストレートフラッシュのための関数
+
+	void P_YAKU();
+
+	void E_CARD_PEA();//1,2ペア判断のための関数
+
+	void E_CARD_3_CARD();//3カードのための関数
+
+	void E_CARD_4_CARD();//3カードのための関数
+
+	void E_CARD_FLASH();//フラッシュのための関数
+
+	void E_CARD_STRAIGHT();//ストレートのための関数
+
+	void E_CARD_STRAIGHTFALSH();//ストレートフラッシュのための関数
+
+	void E_YAKU();
+
+	void WINANDLOSE();
+};
+
---
+++ GameCollect/reversi.h
@@ -0,0 +1,87 @@
+#pragma once
+#include "AbstractScene.h"
+
+enum class Order {
+  First,
+  Second,
+};
+
+
+class Reversi :
+    public AbstractScene
+
+{
+private:
+
+    bool K_Flg;
+    int R_Img;
+    int bor;
+    int B_Storn_Img;
+
+    struct {
+        int Typ[8][8];
+        int X[8][8];
+        int Y[8][8];
+    }Sto;
+    
+    //カーソル
+    struct{
+        int X = 0;
+        int Y = 0;
+    }Cur;
+
+    //フラグ
+    struct {
+        bool Xr = 0;
+        bool Xl = 0;
+        bool Yu = 0;
+        bool Yd = 0;
+        bool button = 0;
+    }Fla;
+
+    struct {
+        struct {
+            int l;
+            int m;
+            int r;
+        }x;
+
+        struct {
+            int u;
+            int m;
+            int d;
+        }y;
+    }e;
+
+    int Tur;
+
+    //POINT CursorPoint;	//カーソルの座標用
+    bool XOnce;	//Lスティック入力重複防止用（横）
+    bool YOnce;	//Lスティック入力重複防止用（縦）
+
+
+
+public:
+    //コンストラクタ
+    Reversi();
+
+    //デストラクタ
+    ~Reversi();
+
+    //描画以外の更新を実行
+    AbstractScene* Update() override;
+
+    //描画に関することを実装
+    void Draw() const override;
+
+   void Cursor();
+   void turn();
+
+
+   int Bac;
+   int Bla;
+   int Whi;
+
+};
+
+
