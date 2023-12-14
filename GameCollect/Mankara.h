ac7f682d83054bb1f3e7d3d6e147914e1c774749
harufumi555 <120348465+harufumi555@users.noreply.github.com>
2023-12-14 13:11:55 +0900

    Merge branch 'harufumi/mankara' into MainAgent

--- GameCollect/Mankara.h
+++ GameCollect/Mankara.h
@@ -15,13 +15,31 @@ private:
 
 	struct STONE
 	{
-		bool flg = 1;
 		int img;
 	} ;
-	// 石の色と各個数
-	struct STONE gStone[6][8];
+	// 石の数とポケット
+	struct STONE gStone[16][6];
 
-	
+	struct STONE2
+	{
+		int img;
+	} ;
+	// 石の数とポケット
+	struct STONE2 gStone2[16][6];
+
+	struct GOAL
+	{
+		int img;
+	} ;
+	// 石の色と個数
+	struct GOAL gGoal[30][2];
+
+
+
+
+
+
+
 
 	// 1Pが選択しているポケット
 	int PlayerPocket;
@@ -37,7 +55,10 @@ private:
 	//1Pのゴール
 	int P1BigPocket; 
 	//2Pのゴール
-	int P2BigPocket; 
+	int P2BigPocket;
+
+	/*int BigPocket[6][40];
+	int BigPocket2[6][40];*/
 
 	// 石の画像
 	int StoneImg[6]; 
@@ -48,183 +69,38 @@ private:
 	int Stone_cnt;
 	int movePocket;
 	int Stone_count;
-	int StonePocket[2][6]; // 石入れてるポケット
+	int StonePocket[12]; // 石入れてるポケット
 	int sideAddition;      // 移動させるための処理
 	int Stonenum;          // 配列の中の石の総数
 
 	int CountUp;
-
-	bool StoneFlg;
 	int moveStone; 
 	int moveStone2;
+	// ポケットの数と格納されている石の数
+	int P1StoneSave[6]; 
 	// ポケットの数と入っている石の数
-	int P1StoneSave[6][4]; 
-	// ポケットの数と入っている石の数
-	int P2StoneSave[6][4]; 
+	int P2StoneSave[6]; 
 
 	int JustGoal; //ぴったりゴール用 
 
+	int MoveStoneFlg; // 動いたかどうか確認するフラグ
+	int MoveStoneFlg2;
+	int MoveStoneFlg3; 
+	int MoveStoneFlg4; 
+	int MoveStoneFlg5; 
+	int MoveStoneFlg6; 
 
+	int MoveStoneFlg7; // 動いたかどうか確認するフラグ
+	int MoveStoneFlg8;
+	int MoveStoneFlg9;
+	int MoveStoneFlg10;
+	int MoveStoneFlg11;
+	int MoveStoneFlg12;
 public:
 	Mankara();
 	AbstractScene* Update();
 	void Draw() const;
 	// 石が移動した時の内部処理
 	void MoveStone();
+	void WinLose();
 };
-
-/*if (P1Turn == 1) {
-	if (P1Pocket[0] == TRUE) {
-		if (Stone_cnt > 0) {
-			StonePocket[1] += Stone_cnt;
-			StonePocket[2] += Stone_cnt;
-			StonePocket[3] += Stone_cnt;
-			StonePocket[4] += Stone_cnt;
-				Stone_cnt-=1;
-		}
-		P2Turn = 1;
-		P1Turn = 0;
-	}
-	else if (P1Pocket[1] == TRUE) {
-		movePocket = 1;
-		if (Stone_cnt > 0) {
-			StonePocket[1 + movePocket] += Stone_cnt;
-			StonePocket[2 + movePocket] += Stone_cnt;
-			StonePocket[3 + movePocket] += Stone_cnt;
-			StonePocket[4 + movePocket] += Stone_cnt;
-			Stone_cnt -= 1;
-		}
-		P2Turn = 1;
-		P1Turn = 0;
-	}
-	else if (P1Pocket[2] == TRUE) {
-		movePocket = 2;
-		if (Stone_cnt > 0) {
-			StonePocket[1 + movePocket] += Stone_cnt;
-			StonePocket[2 + movePocket] += Stone_cnt;
-			StonePocket[3 + movePocket] += Stone_cnt;
-			StonePocket[4 + movePocket] += Stone_cnt;
-			P1BigPocket++;
-			Stone_cnt -= 2;
-		}
-		P2Turn = 0;
-		P1Turn = 1;
-
-	}
-	else if (P1Pocket[3] == TRUE) {
-		movePocket = 3;
-		if (Stone_cnt > 0) {
-			StonePocket[1 + movePocket] += Stone_cnt;
-			StonePocket[2 + movePocket] += Stone_cnt;
-			P1BigPocket++;
-			StonePocket[3 + movePocket] += Stone_cnt;
-			Stone_cnt -= 3;
-		}
-		P2Turn = 1;
-		P1Turn = 0;
-	}
-	else if (P1Pocket[4] == TRUE) {
-		movePocket = 4;
-		if (Stone_cnt > 0) {
-			StonePocket[1 + movePocket] += Stone_cnt;
-			StonePocket[2 + movePocket] += Stone_cnt;
-			P1BigPocket++;
-			StonePocket[3 + movePocket] += Stone_cnt;
-			Stone_cnt -= 4;
-		}
-		P2Turn = 1;
-		P1Turn = 0;
-	}
-	else if (P1Pocket[5] == TRUE) {
-		movePocket = 5;
-		if (Stone_cnt > 0) {
-			StonePocket[1 + movePocket] += Stone_cnt;
-			StonePocket[2 + movePocket] += Stone_cnt;
-			P1BigPocket++;
-			StonePocket[3 + movePocket] += Stone_cnt;
-			Stone_cnt -= 5;
-		}
-		P2Turn = 1;
-		P1Turn = 0;
-	}
-}*/
-
-
-
-//// ２P用石の移動
-//if (P2Turn == 1) {
-
-   // if (P2Pocket[0] == TRUE) {
-   //	 
-   //	 if (Stone_cnt > 0) {
-   //		 StonePocket[7 + movePocket] += Stone_cnt;
-   //		 StonePocket[8 + movePocket] += Stone_cnt;
-   //		 StonePocket[9 + movePocket] += Stone_cnt;
-   //		 StonePocket[10 + movePocket] += Stone_cnt;
-   //		 Stone_cnt -= 1;
-   //	 }
-   //	 P2Turn = 0;
-   //	 P1Turn = 1;
-   // }
-   // else if (P2Pocket[1] == TRUE) {
-   //	 movePocket = 1;
-   //	 if (Stone_cnt > 0) {
-   //		 StonePocket[7 + movePocket] += Stone_cnt;
-   //		 StonePocket[8 + movePocket] += Stone_cnt;
-   //		 StonePocket[9 + movePocket] += Stone_cnt;
-   //		 StonePocket[10 + movePocket] += Stone_cnt;
-   //		 Stone_cnt -= 1;
-   //	 }
-   //	 P2Turn = 0;
-   //	 P1Turn = 1;
-   // }
-   // else if (P2Pocket[2] == TRUE) {
-   //	 movePocket = 2;
-   //	 if (Stone_cnt > 0) {
-   //		 StonePocket[7 + movePocket] += Stone_cnt;
-   //		 StonePocket[8 + movePocket] += Stone_cnt;
-   //		 StonePocket[9 + movePocket] += Stone_cnt;
-   //		 P2BigPocket++;
-   //		 Stone_cnt -= 2;
-   //	 }
-   //	 P2Turn = 0;
-   //	 P1Turn = 1;
-   // }
-   // else if (P2Pocket[3] == TRUE) {
-   //	 movePocket = 3;
-   //	 if (Stone_cnt > 0) {
-   //		 StonePocket[7 + movePocket] += Stone_cnt;
-   //		 StonePocket[8 + movePocket] += Stone_cnt;
-   //		 P2BigPocket++;
-   //		 StonePocket[0] += Stone_cnt;
-   //		 Stone_cnt -= 3;
-   //	 }
-   //	 P2Turn = 1;
-   //	 P1Turn = 0;
-   // }
-   // else if (P2Pocket[4] == TRUE) {
-   //	 movePocket = 4;
-   //	 if (Stone_cnt > 0) {
-   //		 StonePocket[7 + movePocket] += Stone_cnt;
-   //		 P2BigPocket++;
-   //		 StonePocket[0] += Stone_cnt;
-   //		 StonePocket[1] += Stone_cnt;
-   //		 Stone_cnt -= 4;
-   //	 }
-   //	 P2Turn = 0;
-   //	 P1Turn = 1;
-   // }
-   // else if (P2Pocket[5] == TRUE) {
-   //	 movePocket = 5;
-   //	 if (Stone_cnt > 0) {
-   //		 P2BigPocket++;
-   //		 StonePocket[0] += Stone_cnt;
-   //		 StonePocket[1] += Stone_cnt;
-   //		 StonePocket[2] += Stone_cnt;
-   //		 Stone_cnt -= 5;
-   //	 }
-   //	 P2Turn = 0;
-   //	 P1Turn = 1;
-   // }
-
-//}
