3c30e4291403368739273e74cc6d889a2c960111
モトラ２０２２ <sorato-motomura-k22f@stu.kbc.ac.jp>
2024-01-23 13:32:25 +0900

    背景とカーソル画像の追加
    カーソルの移動の位置

--- GameCollect/Connect4.cpp
+++ GameCollect/Connect4.cpp
@@ -1,8 +1,19 @@
 #include "Connect4.h"
+#include"PadInput.h"
 
 Connect4::Connect4()
 {
+	if ((gStageImg = LoadGraph("images/SixBallPazzle/Board2.png")) == -1);
+	if ((gCursorImg = LoadGraph("images/SixBallPazzle/Arrow.png")) == -1);
 
+	//配列の初期化
+	/*for (int x; x < 6; x++) {
+		for (int y; y < 7; y++) {
+			 Board[x][y] = 0;
+		}
+	}*/
+
+	
 }
 
 Connect4::~Connect4()
@@ -11,10 +22,25 @@ Connect4::~Connect4()
 
 AbstractScene* Connect4::Update()
 {
+	if (PAD_INPUT::OnButton(XINPUT_BUTTON_DPAD_RIGHT)) {
+
+		if (++Num > 7) Num = 0;	//右へ
+	}
+
+	if (PAD_INPUT::OnButton(XINPUT_BUTTON_DPAD_LEFT)) {
+
+		if (--Num < 0) Num = 2;	//左へ
+	}
+
+	Cursor = Num * 150;
+	
 	return this;
 }
 
 void Connect4::Draw() const
 {
+	DrawGraph(0, 0, gStageImg, TRUE);	//ステージ画像
+	//DrawGraph(100, 210 + Cursor, 170, 230 + Cursor, 140, 250 + Cursor, 0xff0000, TRUE);	//カーソル画像
+	DrawGraph(150 + Cursor, 0, gCursorImg, TRUE);	//カーソル画像
 
 }
--- GameCollect/Connect4.h
+++ GameCollect/Connect4.h
@@ -1,17 +1,42 @@
 #pragma once
 
 #include "AbstractScene.h"
+#include "DxLib.h"
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+//画面サイズ (縦)→720
+//画面サイズ (横))→1280
+
+//const int HEIGHT = 12;
+//const int WIDTH = 12;
+const int BLOCKSIZE = 48;
+const int MARGIN = 0;
+
 class Connect4 :public AbstractScene{
 
 private:
-	int Board[6][7] = {};
+	int Board[6][7];
 	unsigned int Cr;
 
+	int Cursor;		//カーソル
+	int Num = 0;	//カーソルの位置
+	int gKeyFlg;	//今のキー
+
+	int gStageImg;	//ステージ画像
+	int gCursorImg;	//カーソル画像
+
 public:
+
+	int flg;		//フラグ（０：表示しない　１：表示する）
+	int x, y;		//座標
+	int w, h;		//幅、高さ
+	int img;		//画像番号
+	int backup;		//画像バックアップ
+
+	//struct Block gStage[HEIGHT][WIDTH];
+
 	Connect4();
 
 	~Connect4();
